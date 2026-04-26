cbaae02194e93a5ab4785af0da2584321a26cf7c
Markus Kreukniet <shonof@gmail.com>
2026-04-26 12:59:33 +0200

    added early and late reflections

--- src/audio.js
+++ src/audio.js
@@ -9,16 +9,6 @@ export function createAudio() {
   // 5. no difference between early reflections and late tail (if it is the correct name)
   // 6. short reverbs could use 3 internal delays instead of always 5
 
-  // TODO: comment
-  // The delays should be between 10 ms en 35 ms (10 and 35 is also allowed).
-  // When 4, 5, 6, and 7 in step to 10, we stop at 32 ms.
-  // When we start at 5, 6, 7, 8, then we go too high
-  // it makes more sense to add 4, 6, 5, 7, to make it less math like
-  // After the delays we can add 1 to every number.
-  // Also it is good te prefer 3 delays low, 1 mid, and 1 high
-  // And and there should not be equal steps between the delays
-  const EARLY_REFLECTION_DELAYS = [0.011, 0.015, 0.021, 0.026, 0.033] // TODO: naming
-
   // TODO: this should only happen after user input, now it also happens before < results in warning
   const context = new AudioContext()
   const masterGain = context.createGain()
@@ -45,17 +35,52 @@ export function createAudio() {
     return delayNode
   }
 
-  // TODO: first delay starts not precisely when pre-delay ends, which we can fix with early reflections?
   function createSchroederReverb(preDelaySeconds, decaySeconds) {
+    // TODO: comment
+    // The delays should be between 10 ms and 50 ms (10 and 50 is also allowed).
+    // When we add 7, 8, 9, 10 at 10, we have a better range than we would start with 6 or 8.
+    // Adding like 7, 8, 10, 9 sounds better (less like math).
+    // We can add 3 to every number to space it better between 10 and 50 ms.
+    // Also, the problem of starting at 10 and a 10 interval is then gone (we should avoid equal intervals).
+    // Plus, it is good te prefer 3 delays low, 1 mid, and 1 high.
+    const earlyReflectionDelays = [0.013, 0.02, 0.028, 0.038, 0.047]
+
+    // TODO: comment
+    // The delays should be between 10 ms and 120 ms (10 and 120 is also allowed).
+    // When we add 16, 17, 18, 19 at 50, we have the best range from 50 to 120, but we can't randomize it/can't add stereo offset
+    // Adding subtracting 1 from 50 or adding 1 to 120 makes a delay to short or too long.
+    // When we add 15, 16, 17, 18 at 50, we have a range that we can randomize.
+    // Adding like 15, 16, 18, 17 sounds better (less like math).
+    // We can add 2 to every number to space it better between 50 and 120 ms.
+    // Plus, it is good te prefer 3 delays low, 1 mid, and 1 high.
+    const lateReflectionDelays = [0.052, 0.067, 0.083, 0.101, 0.118]
+
     const reverbIn = context.createGain()
     const reverbOut = context.createGain()
     const combSum = context.createGain()
-
+    const earlyReflections = context.createGain()
     const preDelayNode = context.createDelay()
-    preDelayNode.delayTime.value = preDelaySeconds
+
+    // Early reflections are slightly brighter // TODO: comment
+    earlyReflections.gain.value = 1
+    combSum.gain.value = 0.8
+
+    preDelayNode.delayTime.value = Math.max(0, preDelaySeconds - earlyReflectionDelays[0])
     reverbIn.connect(preDelayNode)
 
-    for (const delay of [0.03, 0.037, 0.041, 0.043]) /* combFilterDelays */ {
+    for (let i = 0; i < earlyReflectionDelays.length; i++) {
+      const delayNode = context.createDelay()
+      const gainNode = context.createGain()
+
+      delayNode.delayTime.value = earlyReflectionDelays[i]
+      gainNode.gain.value = Math.exp(-i * 0.2)
+
+      preDelayNode.connect(delayNode)
+      delayNode.connect(gainNode)
+      gainNode.connect(earlyReflections)
+    }
+
+    for (const delay of lateReflectionDelays) {
       // Maps decay time to per-delay feedback; -3 shapes a natural decay.
       const feedbackCoefficient = Math.exp((-3 * delay) / decaySeconds)
 
@@ -64,8 +89,12 @@ export function createAudio() {
       combFilterNode.connect(combSum)
     }
 
+    const reverbSum = context.createGain() // TODO: naming + placement
+    earlyReflections.connect(reverbSum)
+    combSum.connect(reverbSum)
+
     // highpass first, then lowpass // TODO: comment
-    chainFilters(chainFilters(combSum, 'highpass', 250, 4), 'lowpass', 6000, 1).connect(reverbOut)
+    chainFilters(chainFilters(reverbSum, 'highpass', 250, 4), 'lowpass', 6000, 1).connect(reverbOut)
 
     return {reverbIn, reverbOut} // TODO: is it needed to send object back?
   }
